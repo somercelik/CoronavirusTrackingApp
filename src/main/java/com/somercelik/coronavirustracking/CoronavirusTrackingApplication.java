cb604db5c1c143f099b3bce966389daece29cd3f
Soner Celik 84240320 <soner.celik@huawei.com>
2022-03-31 17:03:35 +0300

    Initial Commit

---
+++ src/main/java/com/somercelik/coronavirustracking/CoronavirusTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.somercelik.coronavirustracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somercelik/coronavirustracking/CoronavirusTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somercelik.coronavirustracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
